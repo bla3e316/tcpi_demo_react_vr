cb6cc3462d456a6ab792780cbdf0959a81fd0a5a
Davis Ford <ford_davis@bah.com>
2017-03-17 14:30:54 -0400

    removed duplicate Team

--- index.vr.js
+++ index.vr.js
@@ -1,16 +1,13 @@
 import React from "react";
 import {AppRegistry, asset, StyleSheet, Pano, Text, View} from "react-vr";
 import Project_Container from "./components/project_container";
-import Team from "./components/team";
 
 class DEF_REACT_VR1 extends React.Component {
   render() {
     return (
         <View>
           <Pano source={asset('desert.jpg')}/>
-
           <Project_Container/>
-          <Team/>
         </View>
     );
   }
